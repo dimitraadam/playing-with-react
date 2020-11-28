b1bafb19287d69cf8e622158e7c157bca605a77c
Dimitra <dimit.adam@gmail.com>
2020-11-28 09:52:58 +0200

    get request for statistics

--- src/components/Home/Statistics.js
+++ src/components/Home/Statistics.js
@@ -1,19 +1,53 @@
-import { Typography, Grid, makeStyles, Paper } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Avatar, Chip, Typography, Grid, makeStyles, Paper } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
-    // root: {
-    //     direction: "row",
-    //     justify: "space-evenly",
-    //     alignItems: "flex-start"
-    // },
     paper: {
         padding: 10
-    }
+    },
+    label: {
+        textTransform: 'uppercase',
+      },
 }));
+
+const API_BASE_URL = "http://localhost:3001";
+const enpointPrefix = `${API_BASE_URL}/stats`;
 
 export default function Statistics() {
 
     const classes = useStyles();
+
+    const [stats, setStats] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = () => {
+            setError(false);
+            setIsLoading(true);
+
+            axios.get(enpointPrefix)
+                .then(response => {
+                    setStats(response.data);
+                    setIsLoading(false);
+                })
+                .catch(error => {
+                    setError(error);
+                    setIsLoading(false);
+                });
+        };
+
+        fetchData();
+    }, []);
+
+    // if (error) {
+    //     return <Alert severity="warning">{error.message}</Alert>;
+    // }
+
+    // if (isLoading) {
+    //     return <div>loading</div>
+    //   }
 
     return (
         <Grid
@@ -21,31 +55,13 @@ export default function Statistics() {
             direction="row"
             justify="space-evenly"
             alignItems="flex-start">
-            <Grid item>
-                <Paper className={classes.paper}>
-                    <Typography>STUDENTS:</Typography>
-                    <Typography>2245</Typography>
-                </Paper>
-
-            </Grid>
-            <Grid item>
-                <Paper className={classes.paper}>
-                    <Typography>COURCES:</Typography>
-                    <Typography>24</Typography>
-                </Paper>
-            </Grid>
-            <Grid item>
-                <Paper className={classes.paper}>
-                    <Typography>INSTRUCTORS:</Typography>
-                    <Typography>2</Typography>
-                </Paper>
-            </Grid>
-            <Grid item>
-                <Paper className={classes.paper}>
-                    <Typography>EVENTS:</Typography>
-                    <Typography>26</Typography>
-                </Paper>
-            </Grid>
+            {stats.map((stat) =>
+                <Grid item>
+                    <Paper className={classes.paper}>
+                        <Typography >{stat.title}</Typography>
+                        <Chip color="primary" label={stat.amount} />
+                    </Paper>
+                </Grid>)}            
         </Grid>
     );
 }
