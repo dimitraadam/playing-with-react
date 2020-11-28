cefbdcf4372ed8e470cfff4b3e64c3ba377c1669
Dimitra <dimit.adam@gmail.com>
2020-11-28 09:51:50 +0200

    apply theme

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import Header from "./components/AppHeader";
 import Home from "./components/Home/Home";
 import CoursesGallery from "./components/AllCourses/CoursesGallery";
 import CourseDetails from "./components/Course/CourseDetails";
-import AddCourse from "./components/NewCourse/AddCourse";
+import CourseForm from "./components/NewCourse/CourseForm";
 import Notfound from "./components/NotFound";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core";
+import lightTheme from "./styles/lightTheme";
 
 function App() {
   return (
     <Router>
-    <Header />    
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/courses" component={CoursesGallery}></Route>
-        <Route exact path="/courses/new" component={AddCourse}></Route>
-        <Route exact path="/courses/:id" component={CourseDetails}></Route>
-        <Route><Notfound /></Route>
-      </Switch>     
+      <MuiThemeProvider theme={createMuiTheme(lightTheme)}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/courses" component={CoursesGallery}></Route>
+          <Route exact path="/courses/new" component={CourseForm}></Route>
+          <Route exact path="/courses/:id" component={CourseDetails}></Route>
+          <Route><Notfound /></Route>
+        </Switch>
+      </MuiThemeProvider>
     </Router>
   );
 }
